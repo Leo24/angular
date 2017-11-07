c589264bcf8f0c524ed241b4f8262d2d501ee222
Vladimir <bitriani@gmail.com>
2017-11-07 16:54:23 +0200

    add crud for heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -35,4 +35,14 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+
 }
